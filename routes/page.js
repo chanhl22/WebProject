e1b7911b33f061dbc35078e8b579bbe45f3483e0
chanhl22 <chanhlee@student.42seoul.kr>
2021-03-25 23:54:21 +0900

    first start nodemon

---
+++ routes/page.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
+const { Post, User, Hashtag } = require('../models');
+
+const router = express.Router();
+
+router.use((req, res, next) => {
+  res.locals.user = req.user;
+  res.locals.followerCount = req.user ? req.user.Followers.length : 0;
+  res.locals.followingCount = req.user ? req.user.Followings.length : 0;
+  res.locals.followerIdList = req.user ? req.user.Followings.map(f => f.id) : [];
+  next();
+});
+
+router.get('/profile', isLoggedIn, (req, res) => {
+  res.render('profile', { title: '내 정보 - NodeBird' });
+});
+
+router.get('/join', isNotLoggedIn, (req, res) => {
+  res.render('join', { title: '회원가입 - NodeBird' });
+});
+
+router.get('/', (req, res) => {
+    res.render('main', { title: 'NodeBird' });
+});
+
+module.exports = router;
